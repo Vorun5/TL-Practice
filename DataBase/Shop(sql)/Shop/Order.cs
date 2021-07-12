34c0e70351b9bb3d82fc98a79e8085b9e416d61a
Vorun5 <80065908+Vorun5@users.noreply.github.com>
2021-07-12 14:35:50 +0300

    Id

--- DataBase/Shop(sql)/Shop/Order.cs
+++ DataBase/Shop(sql)/Shop/Order.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Shop
 {
     class Order
     {
         public int OrderId;
-        public int IdCustomer;
+        public int CustomerId;
         public string ProductName;
         public int Price;
     }
